67176f4893b2d3ab956c26795954dfa5a5061147
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-19 08:12:04 +0900

    Update 17087.cpp

--- 수학1/17087.cpp
+++ 수학1/17087.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int a[100001];
 
-int func(int x, int y) { //gcd공식
+int func(int x, int y) {
 	if (y == 0) return x;
 	else return func(y, x % y);
 }
@@ -11,19 +11,19 @@ int func(int x, int y) { //gcd공식
 int main(int argc, char* argv[])
 {
 	int n, s;
-	
+
 
 	cin >> n >> s;
-	
+
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 		if (a[i] > s)
 			a[i] = a[i] - s;
 		else
-			a[i] = s - a[i]; // abs(s - a[i]);
+			a[i] = s - a[i];
 	}
 
-	int ans = a[0]; // first gcd
+	int ans = a[0];
 
 	for (int i = 1; i < n; i++)
 	{
