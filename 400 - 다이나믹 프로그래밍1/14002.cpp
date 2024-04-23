b7956ed8d2bbd08cf35d4e958598d8399978e51c
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-23 23:47:22 +0900

    Create 14002.cpp

---
+++ 400 - 다이나믹 프로그래밍1/14002.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, i, j;
+	int A[1000], len[1000], ans[1000];
+	cin >> N;
+	for (i = 0; i < N; i++)
+		cin >> A[i];
+	// LIS 구하기
+	for (i = 0; i < N; i++) {
+		len[i] = 1;
+		for (j = 0; j < i; j++) {
+			if (A[i] > A[j])
+				len[i] = max(len[i], len[j] + 1);
+		}
+	}
+	// 길이가 가장 긴 곳의 위치 찾기
+	int maxlen = 0, maxidx;
+	for (i = 0; i < N; i++)
+		if (len[i] > maxlen) {
+			maxlen = len[i];
+			maxidx = i;
+		}
+	// 경로 추적
+	ans[0] = A[maxidx];
+	int l = maxlen - 1;
+	for (i = maxidx - 1, j = 0; i >= 0; i--) {
+		if (len[i] == l && A[i] < ans[j]) {
+			ans[++j] = A[i];
+			l--;
+		}
+	}
+	cout << maxlen << "\n";
+	for (i = maxlen - 1; i >= 0; i--)
+		cout << ans[i] << ' ';
+}
