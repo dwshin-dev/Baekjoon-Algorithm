4ebd25232513b40608acc0f8046df0c9954a8fc7
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-24 23:51:34 +0900

    Update 1912_1.cpp

--- 400 - 다이나믹 프로그래밍1/1912_1.cpp
+++ 400 - 다이나믹 프로그래밍1/1912_1.cpp
@@ -2,13 +2,12 @@
 #include <algorithm>
 using namespace std;
 int a[100001] = { 0, };
-int b[100001] = { 0, };
 int dp[100001] = { 0, };
 
 int main(int argc, char* argv[])
 {
 	int n, ans = 0;
-	cin >> n; 
+	cin >> n;
 	for (int i = 0; i < n; ++i) {
 		cin >> a[i];
 	}
@@ -17,7 +16,7 @@ int main(int argc, char* argv[])
 	dp[0] = a[0];
 
 	for (int i = 1; i < n; ++i) {
-		dp[i] = max(dp[i - 1] + a[i], a[i]);
+		dp[i] = max(dp[i - 1] + a[i], a[i]); //dp[i] = a[i]까지의 부분합중 가장 큰값 과 a[i]값을 비교
 		ans = max(dp[i], ans);
 	}
 	cout << ans << "\n";
