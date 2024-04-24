4e0243f0e1e789bc9d475934c32a78088021b91b
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-24 23:42:07 +0900

    Update 1912.cpp

--- 400 - 다이나믹 프로그래밍1/1912.cpp
+++ 400 - 다이나믹 프로그래밍1/1912.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int a[100001] = { 0, };
-int b[100001] = { 0, };
-int dp[100001] = { 0, };
+int a[100001] = { 0, }; //수를 입력받는 배열
+int b[100001] = { 0, }; //부분합을 저장하기 위한 배열
+int dp[100001] = { 0, }; //dp값을 저장하는 배열
 
 int main(int argc, char* argv[])
 {
 	int n;
 	cin >> n; 
-	int ans = -1001;
+	int ans = -1001; 
 
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
