60d4011d7c8154dfbd1059350e4fee3e13d2cd67
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-25 15:36:25 +0900

    Create 1699.cpp

---
+++ 400 - 다이나믹 프로그래밍1/1699.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[100001] = { 0, };
+
+int main(int argc, char* argv[])
+{
+	int n;
+	cin >> n;
+    for (int i = 0; i <= n; i++) dp[i] = i;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    cout << dp[n];
+
+	return 0;
+}
