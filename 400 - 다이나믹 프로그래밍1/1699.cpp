5f99757a64526a418c3aee1cd87d8da309afd67e
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-25 15:38:48 +0900

    Update 1699.cpp

--- 400 - 다이나믹 프로그래밍1/1699.cpp
+++ 400 - 다이나믹 프로그래밍1/1699.cpp
@@ -10,8 +10,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i <= n; i++) dp[i] = i; // 1만의 합으로 이루어진 구성이라 가정
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j * j <= i; j++) 
-        {
+        for (int j = 1; j * j <= i; j++) {
             dp[i] = min(dp[i], dp[i - j * j] + 1);
         }
     }
