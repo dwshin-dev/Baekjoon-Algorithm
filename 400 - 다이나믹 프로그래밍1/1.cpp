a2eca2ec5f6fb0973e2f0a2ee8d8c92dac047b46
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-26 22:45:20 +0900

    Update 1.cpp

--- 400 - 다이나믹 프로그래밍1/1.cpp
+++ 400 - 다이나믹 프로그래밍1/1.cpp
@@ -7,14 +7,14 @@ int main(int argc, char* argv[])
 {
 	int n;
 	cin >> n;
-    for (int i = 0; i <= n; i++) dp[i] = i;
+    for (int i = 0; i <= n; i++) dp[i] = i;//zz
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j * j <= i; j++) {
             dp[i] = min(dp[i], dp[i - j * j] + 1);
         }
     }
-    cout << dp[n];
+    cout << dp[n];//zzzdk¾Ó
 
 	return 0;
 }
