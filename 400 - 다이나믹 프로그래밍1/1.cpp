d79fc247efe8f1eddb15f5bce647fd8a20265e00
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-04-27 20:37:05 +0900

    Update 1.cpp

--- 400 - 다이나믹 프로그래밍1/1.cpp
+++ 400 - 다이나믹 프로그래밍1/1.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 {
 	int n;
 	cin >> n;
-	for (int i = 0; i <= n; i++) dp[i] = i;//z zz
+	for (int i = 0; i <= n; i++) dp[i] = i;//z 
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j * j <= i; j++) {
