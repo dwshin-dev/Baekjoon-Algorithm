3cdf5beb640acf7ad0dc83e4bf517904238db822
dwshin-dev <122520434+dwshin-dev@users.noreply.github.com>
2024-05-14 18:22:56 +0900

    Create 2309.cpp

---
+++ 500 - 브루트 포스/2309.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a[10], sum = 0;
+	for (int i = 0;i < 9;i++) {
+		cin >> a[i];
+		sum += a[i];
+	}
+
+	sort(a, a + 9);
+
+	for (int i = 0;i < 8;i++) {
+		for (int j = i + 1;j < 9;j++) {
+			if (sum - a[i] - a[j] == 100) {
+				for (int k = 0;k < 9;k++) {
+					if (k == i || k == j) {
+						continue;
+					}
+					cout << a[k] << '\n';
+				}
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
